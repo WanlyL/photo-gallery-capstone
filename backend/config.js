23f19acb09c605103dc79c6aacaaa96d5ccf4c77
WanlyL <138504923+WanlyL@users.noreply.github.com>
2024-05-07 11:42:14 -0400

    updated testing for configuration application

--- backend/config.js
+++ backend/config.js
@@ -10,8 +10,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
 	return process.env.NODE_ENV === 'test'
-		? 'ig_clone_test'
-		: process.env.DATABASE_URL || 'ig_clone';
+		? 'photo_gallery_test'
+		: process.env.DATABASE_URL || 'photo_gallery';
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -19,7 +19,7 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
 
 // colors package allows you to customize your node.js console messages
 //
-console.log('ig_clone Config:'.green);
+console.log('photo_gallery Config:'.green);
 console.log('SECRET_KEY:'.yellow, SECRET_KEY);
 console.log('PORT:'.yellow, PORT.toString());
 console.log('BCRYPT_WORK_FACTOR'.yellow, BCRYPT_WORK_FACTOR);
--- backend/config.test.js
+++ backend/config.test.js
@@ -16,9 +16,9 @@ describe('config can come from env', function () {
 		delete process.env.BCRYPT_WORK_FACTOR;
 		delete process.env.DATABASE_URL;
 
-		expect(config.getDatabaseUri()).toEqual('ig_clone');
+		expect(config.getDatabaseUri()).toEqual('photo_gallery');
 		process.env.NODE_ENV = 'test';
 
-		expect(config.getDatabaseUri()).toEqual('ig_clone_test');
+		expect(config.getDatabaseUri()).toEqual('photo_gallery_test');
 	});
 });
